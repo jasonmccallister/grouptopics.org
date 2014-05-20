6c27c782a85ce080c944c8a53fbc3e76b7f7c95a
Jason McCallister <themccallister@gmail.com>
2014-05-20 09:18:16 -0700

    Add basic frontend methods and routes

--- src/GroupTopics/FrontendBundle/Controller/DefaultController.php
+++ src/GroupTopics/FrontendBundle/Controller/DefaultController.php
@@ -10,4 +10,19 @@ class DefaultController extends Controller
     {
         return $this->render('GroupTopicsFrontendBundle:Default:index.html.twig');
     }
+
+    public function aboutAction()
+    {
+        return $this->render('GroupTopicsFrontendBundle:Default:about.html.twig');
+    }
+
+    public function groupsAction()
+    {
+        return $this->render('GroupTopicsFrontendBundle:Default:groups.html.twig');
+    }
+
+    public function topicsAction()
+    {
+        return $this->render('GroupTopicsFrontendBundle:Default:topics.html.twig');
+    }
 }
